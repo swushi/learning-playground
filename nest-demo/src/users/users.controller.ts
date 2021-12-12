3fbd5af7540ea3071db8d1dd68c686c6c905b927
Sam Autrey <34108400+swushi@users.noreply.github.com>
2021-12-12 15:38:31 -0600

    import logger

--- nest-demo/src/users/users.controller.ts
+++ nest-demo/src/users/users.controller.ts
@@ -9,16 +9,20 @@ import {
   Req,
 } from '@nestjs/common';
 import { Request } from 'express';
+import { LoggerService } from 'src/logger/logger.service';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly logger: LoggerService,
+  ) {}
 
   @Get()
   findAll(@Req() req: Request) {
-    Logger.log(req.url, `${UsersController.name} - ${req.method} ${req.url}`);
+    this.logger.logRequest('', UsersController.name, req);
     return this.usersService.findAll();
   }
 
