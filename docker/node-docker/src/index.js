55764ba902357bfac90cfb907f5dd52663ef1664
Sam Autrey <34108400+swushi@users.noreply.github.com>
2022-01-21 09:23:33 -0600

    got docker example working

--- docker/node-docker/src/index.js
+++ docker/node-docker/src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello Node-Docker!')
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+   res.send("Hello Docker");
+});
+
+app.post("/", (req, res) => {
+   res.send("This is home page with post request.");
+});
+
+// PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+   console.log(`Server is running on PORT: ${PORT}`);
+});
